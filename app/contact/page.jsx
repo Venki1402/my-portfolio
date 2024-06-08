cfc6a2c82041301d08d62f6485c5e92c904c9bd2
Venki1402 <saivenkatesh.alampally@gmail.com>
2024-06-08 10:38:51 +0530

    done with designing contact page

--- app/contact/page.jsx
+++ app/contact/page.jsx
@@ -52,11 +52,13 @@ const Contact = () => {
       <div className="container mx-auto">
         <div className="flex flex-col xl:flex-row gap-[30px] ">
           {/* form */}
-          <div className="xl:h-[54%] order-2 xl:order-none">
+          <div className="xl:w-[54%] order-2 xl:order-none">
             <form className="flex flex-col gap-6 p-10 bg-[#27272c] rounded-xl">
-              <h3 className="text-4xl text-accent">Let{"\'"}s work together!</h3>
+              <h3 className="text-4xl text-accent">Let{"'"}s work together!</h3>
               <p className="text-white/60">
-              I{"\'"}m eager to collaborate on innovative projects and learn from experienced professionals. Let{"\'"}s achieve great results together.
+                I{"'"}m eager to collaborate on innovative projects and learn
+                from experienced professionals. Let{"'"}s achieve great results
+                together.
               </p>
               {/* input */}
               <div className="grid grid-cols-1 md:grid-cols-2 gap-6">
@@ -67,26 +69,47 @@ const Contact = () => {
               </div>
               {/* select */}
               <Select>
-                  <SelectTrigger className="w-full">
-                    <SelectValue placeholder="Select project type"></SelectValue>
-                  </SelectTrigger>
-                  <SelectContent>
-                    <SelectGroup>
-                      <SelectLabel>Select project type</SelectLabel>
-                      <SelectItem value="est">FullStack development</SelectItem>
-                      <SelectItem value="cst">Backend development</SelectItem>
-                      <SelectItem value="mst">Frontend development</SelectItem>
-                      <SelectItem value="wst">Web development</SelectItem>
-                    </SelectGroup>
-                  </SelectContent>
+                <SelectTrigger className="w-full">
+                  <SelectValue placeholder="Select project type"></SelectValue>
+                </SelectTrigger>
+                <SelectContent>
+                  <SelectGroup>
+                    <SelectLabel>Select project type</SelectLabel>
+                    <SelectItem value="est">FullStack development</SelectItem>
+                    <SelectItem value="cst">Backend development</SelectItem>
+                    <SelectItem value="mst">Frontend development</SelectItem>
+                    <SelectItem value="wst">Web development</SelectItem>
+                  </SelectGroup>
+                </SelectContent>
               </Select>
               {/* text-area */}
-              <Textarea className="h-[200px]" placeholder="Type your message here" />
+              <Textarea
+                className="h-[200px]"
+                placeholder="Type your message here"
+              />
+              {/* Button */}
+              <Button size="md" className="max-w-40">
+                Send message
+              </Button>
             </form>
           </div>
           {/* info */}
           <div className="flex-1 flex items-center xl:justify-end order-1 xl:order-none mb-8 xl:mb-0">
-            info
+            <ul className="flex flex-col gap-10">
+              {info.map((item, index) => {
+                return (
+                  <li key={index} className="flex items-center gap-6 mb-6">
+                    <div className="w-[52px] h-[52px] xl:w-[72px] xl:h-[72px] bg-[#27272c] text-accent rounded-md flex items-center justify-center">
+                      <div className="text-[28px] ">{item.icon}</div>
+                    </div>
+                    <div className="flex-1">
+                      <p className="text-white/60">{item.title}</p>
+                      <h3 className="text-xl">{item.value}</h3>
+                    </div>
+                  </li>
+                );
+              })}
+            </ul>
           </div>
         </div>
       </div>
