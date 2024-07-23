28cbc40916476cdaabcb4ee58f7d084dadbf1b30
Venki1402 <saivenkatesh.alampally@gmail.com>
2024-07-23 20:49:11 +0530

    update hire me

--- app/hire/page.jsx
+++ app/hire/page.jsx
@@ -2,7 +2,10 @@ import React from 'react'
 
 const Hire = () => {
   return (
-    <div className='text-9xl flex justify-center items-center py-5'>Can You Afford Me?</div>
+    <div className='text-9xl flex justify-center items-center py-5'>
+      <a href="https://wellfound.com/u/sai-venkatesh-alampally">WellFound</a>
+    </div>
+    
   )
 }
 
