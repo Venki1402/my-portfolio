473d0e0f6291953986e11d0118f72f7d5b89e357
Venki1402 <saivenkatesh.alampally@gmail.com>
2025-02-18 22:03:50 +0530

    remove format to fix styling
    Signed-off-by: Venki1402 <saivenkatesh.alampally@gmail.com>

--- app/projects/page.jsx
+++ app/projects/page.jsx
@@ -189,8 +189,7 @@ const Projects = () => {
               <WorkSliderButtons
                 containerStyles="flex gap-2 absolute right-0 bottom-[calc (50%_ - _22px)] xl:bottom-0 z-20 w-full justify-between xl:w-max
                 xl:justify-none"
-                btnStyles="bg-accent hover:bg-accent-hover text-primary text-[22px] w-
-                [44px] h-[44px] flex justify-center items-center transition-all"
+                btnStyles="bg-accent hover:bg-accent-hover text-primary text-[22px] w-[44px] h-[44px] flex justify-center items-center transition-all"
               />
             </Swiper>
           </div>
